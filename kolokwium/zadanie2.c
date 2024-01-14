ffea4037d1ce8d0e92786a4849da6e168a5f54c0
Konrad <k.petrenkogoljanek.068@studms.ug.edu.pl>
2024-01-14 10:02:57 +0100

    updt

--- kolokwium/zadanie2.c
+++ kolokwium/zadanie2.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 char out;
-void modify(char modifier, char o, char i);
+
+void modify(char modifier, char * p, char i);
 void main()
 {
+    char * pnt;
+    pnt = &out;
     char ins;
     char modifier;
     printf("Podaj znak do zmiany zmiennych\n");
     scanf("%c",&modifier);
-    modify(modifier,out,ins);
+    modify(modifier,pnt,ins);
     exit(EXIT_SUCCESS);
 
 }
-void modify(char modifier,char o,char i)
+void modify(char modifier,char * p,char i)
 {
-    o = modifier;
+    *p = modifier;
     i = modifier;
-    printf("zewnętrzna: %c, wewnętrzna: %c\n",o,i);
+    printf("zewnętrzna: %c, wewnętrzna: %c\n",out,i);
 }
